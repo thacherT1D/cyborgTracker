dc5f59ad6a6d41bbe598cb0e91625d2d5aa68fc7
THACHER <thachert1d@gmail.com>
2016-11-08 22:37:44 -0800

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cyborgTrackerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cyborgTrackerApp
+ */
+angular.module('cyborgTrackerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cyborgTrackerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cyborgTrackerApp
+ */
+angular.module('cyborgTrackerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
